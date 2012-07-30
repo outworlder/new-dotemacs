1ce2cf3576c8c749446bf7cb8f9e2f0b837c5b2c
Stephen Eilert <stephen@guildahq.com>
2012-07-30 13:05:55 -0300

    Fixing parentheses highlight.

--- modules/lisp.el
+++ modules/lisp.el
@@ -1,9 +1,12 @@
+
+(defun lisp-stuff ()
+  (paredit-mode)
+  (dotemacs-maybe-require 'highlight-parentheses
+			  (highlight-parentheses-mode)))
+
 (autoload 'paredit-mode "paredit"
   "Minor mode for pseudo-structurally editing Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       (lambda () (paredit-mode)))
-(add-hook 'lisp-mode-hook             (lambda () (paredit-mode)))
-(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode)))
-(add-hook 'scheme-mode-hook           (lambda () (paredit-mode)))
-
-(dotemacs-maybe-require 'highlight-parentheses
-			(highlight-parentheses-mode))
+(add-hook 'emacs-lisp-mode-hook       (lambda () (lisp-stuff)))
+(add-hook 'lisp-mode-hook             (lambda () (lisp-stuff)))
+(add-hook 'lisp-interaction-mode-hook (lambda () (lisp-stuff)))
+(add-hook 'scheme-mode-hook           (lambda () (lisp-stuff)))
