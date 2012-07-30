821a441ffb0292c62b2ada7b3cdd6649a5940020
Stephen Eilert <stephen@guildahq.com>
2012-07-30 12:49:12 -0300

    Adding highlight-parentheses-mode (mostly for Lisp code)

--- modules/lisp.el
+++ modules/lisp.el
@@ -4,3 +4,6 @@
 (add-hook 'lisp-mode-hook             (lambda () (paredit-mode)))
 (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode)))
 (add-hook 'scheme-mode-hook           (lambda () (paredit-mode)))
+
+(dotemacs-maybe-require 'highlight-parentheses
+			(highlight-parentheses-mode))
