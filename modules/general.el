913c718cd6958cb50212c891519d47cfdf70bc8d
Stephen Eilert <stephen@guildahq.com>
2012-07-31 20:26:58 -0300

    Some more customizations: highlighting the current line, using a 'smarter' tab key and binding F6 to 'compile

--- modules/general.el
+++ modules/general.el
@@ -16,3 +16,24 @@
 
 ;;; Enabling the server
 (server-start)
+
+(hl-line-mode 1)
+
+(global-set-key [(tab)] 'smart-tab)
+(defun smart-tab ()
+  "This smart tab is minibuffer compliant: it acts as usual in
+    the minibuffer. Else, if mark is active, indents region. Else if
+    point is at the end of a symbol, expands it. Else indents the
+    current line."
+  (interactive)
+  (if (minibufferp)
+      (unless (minibuffer-complete)
+        (dabbrev-expand nil))
+    (if mark-active
+        (indent-region (region-beginning)
+                       (region-end))
+      (if (looking-at "\\_>")
+          (complete-symbol nil)
+        (indent-for-tab-command)))))
+
+(global-set-key (kbd "<f5>") 'compile)
