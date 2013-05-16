520e7731a44626489b0d8c8f2d851ac1c606e126
Stephen Eilert <stephen.eilert@codeminer42.com>
2013-05-16 15:57:11 -0300

    Chicken Scheme modes

--- modules/lisp.el
+++ modules/lisp.el
@@ -1,35 +1,31 @@
 ;;; Lisp-like languages customizations
+(dotemacs-add-support "auto-complete-1.3.1")
+(dotemacs-add-support "slime-2012-07-31")
 
-(defun lisp-stuff ()
-  (paredit-mode)
+(dotemacs-maybe-require 'chicken-scheme)
+
+(defun dotemacs-lisp-stuff ()
+  (interactive)
+  (dotemacs-maybe-require 'paredit
+		 (paredit-mode))
   (dotemacs-maybe-require 'highlight-parentheses
 			  (highlight-parentheses-mode)))
 
-(autoload 'paredit-mode "paredit"
-  "Minor mode for pseudo-structurally editing Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       (lambda () (lisp-stuff)))
-(add-hook 'lisp-mode-hook             (lambda () (lisp-stuff)))
-(add-hook 'lisp-interaction-mode-hook (lambda () (lisp-stuff)))
-(add-hook 'scheme-mode-hook           (lambda () (lisp-stuff)))
+(add-hook 'emacs-lisp-mode-hook       'dotemacs-lisp-stuff)
+(add-hook 'lisp-mode-hook             'dotemacs-lisp-stuff)
+(add-hook 'lisp-interaction-mode-hook 'dotemacs-lisp-stuff)
+(add-hook 'scheme-mode-hook           'dotemacs-lisp-stuff)
+
 
 ;;; Slime!
 
-;; (dotemacs-add-support "slime-2012-07-31")
-
-;; (require 'slime)
-;; (require 'slime-autoloads)
-;; (slime-setup '(slime-fancy slime-banner))
-
-;;; Scheme customizations
-
-(dotemacs-add-support "auto-complete-1.3.1")
-
-(require 'chicken-scheme)
-
-;(autoload 'chicken-slime "chicken-slime" "Swank backend for Chicken" t)
+(dotemacs-maybe-require 'slime
+	       (require 'slime)
+	       (require 'slime-autoloads)
+	       (slime-setup '(slime-fancy slime-banner))
+	       (autoload 'chicken-slime "chicken-slime" "Swank backend for Chicken" t))
 
 ;;; TODO: Check if this is necessary
-
 (add-hook 'scheme-mode-hook
 	  (lambda ()
 	    (setq slime-csi-path "/usr/local/bin/csi")
